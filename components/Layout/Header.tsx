0eab69893f00c4211c2891ce40900298c6f0d2c0
Scott Macdonald <scott.scotm@gmail.com>
2024-08-25 10:17:29 +0100

    Clean up Header component use

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 
-import Link from "next/link";
 import { getServerAuthSession } from "~/server/auth";
+import { SignInOrOut } from "../signInOrOut";
 
 const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "Home", href: "/" },
+  { name: "Exercises", href: "/exercises" },
 ];
 
 export default async function Header() {
@@ -20,7 +18,7 @@ export default async function Header() {
         className="flex items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">FitForge</span>
             <img alt="" src="/apple-touch-icon.png" className="h-8 w-auto" />
           </a>
@@ -36,14 +34,8 @@ export default async function Header() {
             </a>
           ))}
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link
-            href={session ? "/api/auth/signout" : "/api/auth/signin"}
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            {session ? "Sign out" : "Sign in"}
-            <span aria-hidden="true">&rarr;</span>
-          </Link>
+        <div className="lg:flex lg:flex-1 lg:justify-end">
+          <SignInOrOut session={session} />
         </div>
       </nav>
     </header>
--- components/signInOrOut.tsx
+++ components/signInOrOut.tsx
@@ -6,7 +6,7 @@ export const SignInOrOut = ({ session }: { session: Session | null }) => {
   return (
     <Link
       href={session ? "/api/auth/signout" : "/api/auth/signin"}
-      className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+      className="text-sm font-semibold leading-6 text-gray-900"
     >
       {session ? "Sign out" : "Sign in"}
     </Link>
