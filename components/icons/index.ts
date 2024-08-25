df1c940e267b6d54c19ccc6f40050f3995f0e1dc
Scott Macdonald <scott.scotm@gmail.com>
2024-08-25 10:06:32 +0100

    update social icons

---
+++ components/icons/index.ts
@@ -0,0 +1,5 @@
+export { Facebook } from "./Facebook";
+export { GitHub } from "./GitHub";
+export { Instagram } from "./Instagram";
+export { Twitter } from "./Twitter";
+export { YouTube } from "./YouTube";
